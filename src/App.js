eef02f8e7839871b563c700f71716f03902758b7
MLaeeqKhan <lk7715714@gmail.com>
2024-03-12 21:04:34 +0500

    Chart.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import { Chart as ChartJS,defaults } from "chart.js/auto";
+import {Bar,Doughnut, Line} from "react-chartjs-2";
+import sourceData from "./data/chartData.json";
+import revenueData from "./data/revenueData.json";
+defaults.maintainAspectRatio=false
+defaults.responsive=true;
+
+defaults.plugins.title.display= true;
+defaults.plugins.title.align="start";
+defaults.plugins.title.font.size=20;
+defaults.plugins.title.color="black"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    < >
+    <div className="line" style={{width:"30rem",height:"20rem"}}>
+      <Line
+      data={
+        {
+          labels:revenueData.map((data)=>data.label),
+          datasets:[
+            {
+              label:"Revenue",
+              data:revenueData.map((data)=>data.reveue),
+              backgroundColor:"#C70039",
+              borderColor:"#C70039"
+            },
+            {
+              label:"Cost",
+              data:revenueData.map((data)=>data.cost),
+              backgroundColor:"#FFC300",
+              borderColor:"#FFC300"
+            }
+          ],
+         
+        }
+      }
+      options={{
+        elements:{
+        line:{
+          tension:0.5
+        }
+        },
+        plugins:{
+          title:{
+
+            text:"Monthly Revenue & Cost"
+          }
+        }
+      }}
+      />
+      
     </div>
+      <div className="bar" style={{width:"30rem",height:"20rem",margin:"20px"}}>
+        <Bar
+        data={{
+          labels: sourceData.map((data)=>data.label),
+          datasets:[
+           { 
+            label:"Count",
+          data: sourceData.map((data)=>data.value),
+          backgroundColor:[
+            "#C70039",
+            "#FFC300",
+            "#581845"
+          ],
+          
+          }
+          ]
+        }}
+        options={{
+          plugins:{
+            title:{
+  
+              text:"Monthly Revenue & Cost"
+            }
+          }
+        }}
+        />
+      </div>
+
+      <div className="bar" style={{width:"30rem",height:"20rem"}}>
+        <Doughnut
+        data={{
+          labels: sourceData.map((data)=>data.label),
+          datasets:[
+           { 
+            label:"Count",
+          data: sourceData.map((data)=>data.value),
+          }
+          ]
+        }}
+        />
+      </div>
+    
+    </>
   );
 }
 
